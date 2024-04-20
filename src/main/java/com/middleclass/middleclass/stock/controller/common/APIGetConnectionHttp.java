e7019a5156b9b36843f6c8f9e486b75604b200a2
hyunzero <garden940316@gmail.com>
2024-04-20 16:21:58 +0900

    Revert "asd"
    This reverts commit 72748be157fb1f40ca7687a6a132f9ba63518390.

--- src/main/java/com/middleclass/middleclass/stock/controller/common/APIGetConnectionHttp.java
+++
@@ -1,9 +0,0 @@
-package com.middleclass.middleclass.stock.controller.common;
-
-import java.io.IOException;
-import java.util.Map;
-
-@FunctionalInterface
-public interface APIGetConnectionHttp {
-    Map<String, Object> getApi(String UrlData, String ParamData, String TrId) throws IOException;
-}
--- src/main/java/com/middleclass/middleclass/stock/controller/domestic/DomesticController.java
+++ src/main/java/com/middleclass/middleclass/stock/controller/domestic/DomesticController.java
@@ -2,7 +2,6 @@ package com.middleclass.middleclass.stock.controller.domestic;
 
 import com.google.gson.Gson;
 import com.middleclass.middleclass.stock.common.TrId;
-import com.middleclass.middleclass.stock.controller.common.APIGetConnectionHttp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("domestic")
-public class DomesticController implements APIGetConnectionHttp {
+public class DomesticController {
     @Value("#{key['stock.token']}")
     private String TOKEN;
 
@@ -37,7 +36,7 @@ public class DomesticController implements APIGetConnectionHttp {
     @Value("#{db['db.url']}")
     private String PW;
 
-    // 거 방법 매우 많네
+    // 거 방법 존나 많네
     // @ConfigurationProperties
     // @PropertySource
 
@@ -47,7 +46,7 @@ public class DomesticController implements APIGetConnectionHttp {
         String url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-price";
         String tr_id = TrId.FHKST01010100.getValue();
         String data = "?fid_cond_mrkt_div_code=J" + "&fid_input_iscd=005930"; // FID 입력 종목코드 + FID 조건 시장 분류 코드
-        Map<String, Object> map = getApi(url, data, tr_id);
+        Map<String, Object> map = httpGetConnection(url, data, tr_id);
         ModelAndView mv = new ModelAndView();
         mv.setViewName("stockPrice");
         mv.addObject("responseCode", map.get("responseCode"));
@@ -56,7 +55,7 @@ public class DomesticController implements APIGetConnectionHttp {
     }
 
     // TODO 이거 공통 모듈로 만들기
-    public Map<String, Object> getApi(String UrlData, String ParamData, String TrId) {
+    public Map<String, Object> httpGetConnection(String UrlData, String ParamData,String TrId) throws IOException {
         String totalUrl = totalUrl = UrlData.trim().toString();
 
         URL url = null;
@@ -71,13 +70,22 @@ public class DomesticController implements APIGetConnectionHttp {
         try {
             url = new URL(totalUrl + ParamData);
             conn = (HttpURLConnection) url.openConnection();
-            // header 정보 세팅, Content-Type 제외하고 다 필수 항목임
+            // header 정보 세팅, Content-Type 제외하고 필수 아닌 항목은 주석 처리
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setRequestProperty("authorization", "Bearer " + TOKEN);
             conn.setRequestProperty("appKey", APPKEY);
             conn.setRequestProperty("appSecret", BEARER_TOKEN);
+            // conn.setRequestProperty("personalSeckey", "{personalSeckey}");
             conn.setRequestProperty("tr_id", TrId);
+            // conn.setRequestProperty("tr_cont", " ");
+            // conn.setRequestProperty("custtype", "법인(B), 개인(P)");
+            // conn.setRequestProperty("seq_no", "법인(01), 개인( )");
+            // conn.setRequestProperty("mac_address", "{Mac_address}");
+            // conn.setRequestProperty("phone_num", "P01011112222");
+            // conn.setRequestProperty("ip_addr", "{IP_addr}");
+            // conn.setRequestProperty("hashkey", "{Hash값}");
+            // conn.setRequestProperty("gt_uid", "{Global UID}");
             conn.setDoOutput(true);
             conn.connect();
 
@@ -111,4 +119,66 @@ public class DomesticController implements APIGetConnectionHttp {
             }
         }
     }
+
+    @GetMapping("chkholiday")
+    public void chkholiday() throws IOException {
+        String url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/chk-holiday";
+        String tr_id = TrId.CTCA0903R.getValue();
+        String data = "?BASS_DT=20240401&CTX_AREA_NK=&CTX_AREA_FK=";
+        this.httpGetConnection_chkholiday(url, data, tr_id);
+    }
+
+    // TODO 이거 공통 모듈로 만들기
+    public Map<String, Object> httpGetConnection_chkholiday(String UrlData, String ParamData,String TrId) throws IOException {
+        String totalUrl = totalUrl = UrlData.trim().toString();
+
+        URL url = null;
+        HttpURLConnection conn = null;
+
+        String responseData = "";
+        BufferedReader br = null;
+
+        StringBuffer sb = null;
+        String returnData = "";
+
+        try {
+            url = new URL(totalUrl + ParamData);
+            conn = (HttpURLConnection) url.openConnection();
+
+            // header 정보(필수인 것들만 세팅)
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("authorization", "Bearer " + TOKEN);
+            conn.setRequestProperty("appKey", APPKEY);
+            conn.setRequestProperty("appSecret", BEARER_TOKEN);
+            conn.setRequestProperty("tr_id", TrId);
+            conn.setRequestProperty("custtype", "P");
+            conn.setDoOutput(true);
+            conn.connect();
+
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        } catch (IOException e) {
+            br = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
+        } finally {
+            try {
+                sb = new StringBuffer();
+                while ((responseData = br.readLine()) != null) {
+                    sb.append(responseData);
+                }
+                returnData = sb.toString();
+                String responseCode = String.valueOf(conn.getResponseCode());
+
+                System.out.println("http 응답 코드 : " + responseCode);
+                System.out.println("http 응답 데이터 : " + returnData);
+
+                if (br != null){
+                    br.close();
+                }
+            } catch (IOException e){
+                throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+            }
+        }
+        return null;
+    }
 }
+
